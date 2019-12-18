5f32865d43578ee0107ec14182adea2c8efe2963
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-12-18 19:10:06 +0100

    Add handling of click on PipelineCard
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import logo from './images/logos/logo-transparent.png';
 
-import PipelineCard from './components/pipeline/PipelineCard';
-import PipelineBody from './components/pipeline/PipelineBody';
+import Pipeline from './components/pipeline/Pipeline';
 
 const getData = () => [
   {x: 0, y: 10*Math.random()},
@@ -113,31 +112,7 @@ export default function() {
 
       <div className="container">
 
-        <div className="d-sm-flex align-items-center justify-content-between mb-4">
-          <h1 className="h3 mb-0 text-gray-800">Lead Time</h1>
-        </div>
-
-        <div className="row mb-4">
-          {
-            pipeline.map(
-              (card, i) =>
-                <div className="col-md-2" key={ i }>
-                  <PipelineCard
-                    title={ card.tab.title }
-                    color={ card.tab.color }
-                    data={ card.tab.data }
-                  >
-                  </PipelineCard>
-                </div>
-            )
-          }
-        </div>
-
-        <PipelineBody
-          section={ pipeline[0].tab.title }
-          charts={ pipeline[0].body.charts }
-        >
-        </PipelineBody>
+        <Pipeline pipeline={ pipeline }></Pipeline>
 
       </div>
 
---
+++ src/components/pipeline/Pipeline.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import PipelineCard from './PipelineCard';
+import PipelineBody from './PipelineBody';
+
+class Pipeline extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {activeCard: 0};
+  }
+
+  render = () => {
+    const pipeline = this.props.pipeline;
+
+    return [
+      <div className="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 className="h3 mb-0 text-gray-800">Lead Time</h1>
+      </div>,
+      <div className="row mb-4">
+        {
+          pipeline.map(
+            (card, i) =>
+              <div className="col-md-2" key={ i }>
+                <PipelineCard
+                  title={ card.tab.title }
+                  color={ card.tab.color }
+                  data={ card.tab.data }
+                  onClick={(e) => this.onPipelineCardClick(i)}
+                >
+                </PipelineCard>
+              </div>
+          )
+        }
+      </div>,
+      <PipelineBody
+        section={ pipeline[this.state.activeCard].tab.title }
+        charts={ pipeline[this.state.activeCard].body.charts }
+      >
+      </PipelineBody>
+    ];
+  }
+
+  onPipelineCardClick = (index) => {
+    console.log("clicked " + index);
+    this.setState({activeCard: index});
+  }
+
+}
+
+export default Pipeline;
