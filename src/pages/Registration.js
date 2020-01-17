ea48b2e346ee2fc34b584d1461cfff061ef4bc90
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2020-01-17 13:31:38 +0100

    Attach handler for login to login button in registration page
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 import logo from '../images/logos/logo-transparent.png';
 
+import { useHistory } from "react-router-dom";
+import { useAuth0 } from "../react-auth0-spa";
+
 export default () => {
+  const history = useHistory();
+  const { loading, isAuthenticated, loginWithRedirect } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isAuthenticated) {
+    history.push("/");
+  }
+
   return (
     <div className="container">
       <div className="row mt-5">
@@ -12,7 +26,7 @@ export default () => {
       </div>
       <div className="row mt-5">
         <div className="col-12 text-center">
-          <button type="button" className="btn btn-primary btn-lg">Login</button>
+          <button type="button" onClick={() => loginWithRedirect({})} className="btn btn-primary btn-lg">Login</button>
         </div>
       </div>
     </div>
