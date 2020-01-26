3855681844a89750f3689b2a8a595cb543106c8c
David Pordomingo <David.Pordomingo.F@gmail.com>
2020-01-26 19:45:59 +0100

    Use 'classnames' module to build classnames

--- src/js/components/pipeline/PipelineCard.jsx
+++ src/js/components/pipeline/PipelineCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classnames from 'classnames';
 
 import CleanAreaChart, { vertical } from 'js/components/charts/CleanAreaChart';
 
@@ -12,7 +13,7 @@ export default ({ title, text, badge, color, data, active, onClick }) => {
   };
 
   return (
-    <div className={"card shadow pipeline-thumbnail" + (active ? ' active' : '')} onClick={onClick} style={cardStyle}>
+    <div className={classnames('card shadow pipeline-thumbnail', active && 'active')} onClick={onClick} style={cardStyle}>
       <div className="card-body">
         <div className="row no-gutters card-title text-xs text-uppercase mb-3">
           <div className="col font-weight-bold">{title}</div>
