37d5c7167642d8284d581417cda8f63617c8138e
David Pordomingo <David.Pordomingo.F@gmail.com>
2020-01-29 08:40:01 +0100

    Enable Info tooltip on compomonent mount

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import $ from 'jquery';
 import 'bootstrap/dist/js/bootstrap.js';
 
 import 'sass/main.scss';
 
 require('js/App.jsx');
-
-$('[data-toggle="tooltip"]').tooltip();
--- src/js/components/ui/Info.jsx
+++ src/js/components/ui/Info.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import $ from 'jquery';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 
-export default ({ content }) => (
-    <span data-toggle="tooltip" data-placement="bottom" title={content} className="ml-2">
-        <FontAwesomeIcon icon={faInfoCircle} className="text-secondary" />
-    </span>
-);
+export default ({ content }) => {
+    useEffect(() => {
+        $('[data-toggle="tooltip"]').tooltip && $('[data-toggle="tooltip"]').tooltip();
+    }, [])
+
+    return (
+        <span data-toggle="tooltip" data-placement="bottom" title={content} className="ml-2">
+            <FontAwesomeIcon icon={faInfoCircle} className="text-secondary" />
+        </span>
+    );
+};
