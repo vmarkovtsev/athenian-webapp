6c1203fe4ad245f382c495270185426fb07cef43
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2020-01-30 18:24:40 +0100

    Call api metricsLine and getUser using the token
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/js/pages/templates/Page.jsx
+++ src/js/pages/templates/Page.jsx
@@ -3,18 +3,31 @@ import React, { useState, useEffect } from 'react';
 import Navbar from 'js/components/layout/Navbar';
 import Footer from 'js/components/layout/Footer';
 
-import { getUser } from 'js/services/api';
+import { getUser, fetchApi } from 'js/services/api';
 import { useAuth0 } from 'js/services/react-auth0-spa';
 
 export default ({ children }) => {
+  const { loading, isAuthenticated, getTokenSilently } = useAuth0();
+
   const [userState, setUser] = useState(null);
-  const { isAuthenticated } = useAuth0();
 
   useEffect(() => {
-    if (isAuthenticated) {
-      getUser().then(setUser);
-    }
-  }, []);
+    const callAPI = async () => {
+      if (loading || !isAuthenticated) {
+        return;
+      };
+
+      getTokenSilently()
+        .then(token => {
+          return fetchApi(token, getUser);
+        })
+        .then(data => {
+          setUser();
+        });
+    };
+
+    callAPI();
+  }, [loading, isAuthenticated, getTokenSilently]);;
 
   return (
     <>
