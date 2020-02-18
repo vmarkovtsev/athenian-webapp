c4121cd6381a64dff9721f9889ff457efb7db3b1
David Pordomingo <David.Pordomingo.F@gmail.com>
2020-02-18 12:59:19 +0100

    Add palettes

---
+++ src/js/res/palette.js
@@ -0,0 +1,16 @@
+export const palette = {
+    stages: {
+        wip: '#FF732B',
+        wipAlt: '#FD9F11',
+        review: '#FFC30F',
+        reviewAlt: '#FFC71E',
+        merge: '#9060E2',
+        mergeAlt: '#CCB9EC',
+        release: '#35CB70',
+        releaseAlt: '#BCE6CE',
+    },
+    charts: {
+        primary: '#000080',
+        secondary: '#FF8C00',
+    },
+};
