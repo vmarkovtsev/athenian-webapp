9fa88268a35f07111aff2ecd2eebdc758b354c02
David Pordomingo <David.Pordomingo.F@gmail.com>
2020-02-23 11:29:04 +0100

    Make MainMetrics component configurable, and use some real data

--- src/js/pages/pipeline/Body.jsx
+++ src/js/pages/pipeline/Body.jsx
@@ -9,6 +9,7 @@ import MainMetrics from 'js/components/pipeline/MainMetrics';
 import Thumbnails from 'js/components/pipeline/Thumbnails';
 
 import { getPipelineDataInitial, getPipelineDataAPI, fetchApi } from 'js/services/api';
+import { usePRsContext } from 'js/context/PRs';
 
 export default ({ children }) => {
     const { getTokenSilently } = useAuth0();
@@ -19,6 +20,7 @@ export default ({ children }) => {
     const activeStageState = pipelineState.findIndex(stage => stage.tab.slug === name);
 
     const { dateInterval, repositories, contributors } = useFiltersContext();
+    const { prs } = usePRsContext();
 
     useEffect(() => {
         if (!userContext || !repositories.length) {
@@ -32,9 +34,34 @@ export default ({ children }) => {
 
     return (
         <>
-            <MainMetrics />
+            {withMainMetrics(prs, dateInterval)}
             <Thumbnails stages={pipelineState} activeCard={activeStageState} />
             {children}
         </>
+    );
+};
+
+const withMainMetrics = (prs, dateInterval) => {
+    const prCount = prs.filter(pr => (pr.created >= dateInterval.from && pr.created <= dateInterval.to)).length;
+    const participants = prs.reduce((acc, pr) => {
+        return [...pr.participants, ...pr.creators].reduce((dict, user) => {
+            dict[user] = true;
+            return dict;
+        }, acc);
+    }, {})
+
+    const contribCount = Object.keys(participants).length;
+
+    const leadTimeAvg = 33; // TODO(dpordomingo): avg to be calculated in another PR
+    const leadTimeVariation = 20; //TODO(dpordomingo): variations to be calculated in another PR
+    const createdPRsVariation = -5; // TODO(dpordomingo): variations to be calculated in another PR
+    const contribsVariation = 10; // TODO(dpordomingo): variations to be calculated in another PR
+
+    return (
+        <MainMetrics
+            leadTimeAvg={leadTimeAvg} leadTimeVariation={leadTimeVariation}
+            createdPRsAvg={prCount} createdPRsVariation={createdPRsVariation}
+            contribsAvg={contribCount} contribsVariation={contribsVariation}
+        />
     );
 };
