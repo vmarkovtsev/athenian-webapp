a44108267c20a7b1cf4ef9922dab901d6dbb81db
David Pordomingo <David.Pordomingo.F@gmail.com>
2020-02-23 11:28:28 +0100

    Prepare Typography components to receive null values
    If nothing is sent, it will render a whitespace (to reserve its space)

--- src/js/components/ui/Typography.jsx
+++ src/js/components/ui/Typography.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import classnames from 'classnames';
 
+export const nbsp = '\u00A0';
+
 export const BigNumber = ({ content }) => (
-    <span className="text-lg font-weight-bold d-inline-block align-middle text-gray-900">{content}</span>
+    <span className="text-lg font-weight-bold d-inline-block align-middle text-gray-900">{content || nbsp}</span>
 );
 
 export const SmallTitle = ({ content, isBlack }) => (
-    <span className={classnames('font-weight-bold text-xs text-uppercase', isBlack && 'text-gray-900')}>{content}</span>
+    <span className={classnames('font-weight-bold text-xs text-uppercase', isBlack && 'text-gray-900')}>{content || nbsp}</span>
 );
