ee19766d8eb332b7e11ac304a4a5ea03d8ec0b35
David Pordomingo <David.Pordomingo.F@gmail.com>
2020-03-01 18:57:44 +0100

    Configure Simple template to enable/disable home link

--- src/js/pages/templates/Simple.jsx
+++ src/js/pages/templates/Simple.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import logo from 'images/logos/logo-transparent.png';
 
-export default ({ children }) => {
+export default ({ children, linkToHome = true }) => {
     return (
         <div className="container">
             <div className="row mt-5">
@@ -14,11 +14,13 @@ export default ({ children }) => {
             <div className="row mt-5">
                 <div className="col-12 text-center">
                     {children}
-                    <div className="mt-4">
-                        <Link to="/">
-                            &lt; Back to Home
+                    {linkToHome && (
+                        <div className="mt-4">
+                            <Link to="/">
+                                &lt; Back to Home
                         </Link>
-                    </div>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
