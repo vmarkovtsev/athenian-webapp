7bc4199c3d7892815e9aeab19c3cf37c612b9e1c
Zuri Negrín <zurinegrin@gmail.com>
2020-03-02 17:54:26 +0100

    Fixing margins, borders and other minor improvements on the pipeline
    Signed-off-by: Zuri Negrín <zurinegrin@gmail.com>

--- src/js/components/pipeline/MainMetrics.jsx
+++ src/js/components/pipeline/MainMetrics.jsx
@@ -12,7 +12,7 @@ export default ({
     contribsAvg, contribsVariation,
 }) => {
     return (
-        <div className="row mb-3">
+        <div className="row mb-4">
             <div className="col-md-4">
                 <MainMetric title="Lead time" value={leadTimeAvg && dateTime.human(leadTimeAvg)} variation={leadTimeVariation}
                     hint="Average of time elapsed between the creation of the 1st commit in the Pull Requests within the active filters, and the code being used in production"
@@ -35,7 +35,7 @@ export default ({
 
 const MainMetric = ({ title, hint, value, variation }) => (
     <div className="card">
-        <div className="card-body">
+        <div className="card-body py-2 px-3">
             <div className="card-title mb-0">
                 <div className="d-flex justify-content-between align-items-center">
                     <div className="font-weight-bold text-uppercase text-xs">
--- src/js/components/pipeline/TopFilter.jsx
+++ src/js/components/pipeline/TopFilter.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default ({ reposFilter, contribsFilter, dateIntervalFilter }) => {
     return (
-        <div className="filters row mb-3">
+        <div className="filters row mb-4">
             <div className="col-12">
                 <div style={{ maxWidth: '300px' }}>
                 </div>
-                <div className="d-flex justify-content-between align-items-center pb-3 border-bottom">
+                <div className="d-flex justify-content-between align-items-center pb-4 border-bottom">
                     <div className="form-row">
                         <div className="col text-xs">
                             {reposFilter}
