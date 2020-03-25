25d2e8ecf95c5889a3b4b58cf4a9ee4fb605dd99
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2020-03-25 13:12:17 +0100

    Add definition for chart "Pull Request Ratio Flow"
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/js/components/insights/stages/WorkInProgress.jsx
+++ src/js/components/insights/stages/WorkInProgress.jsx
@@ -1,11 +1,139 @@
 import { SimpleKPI } from 'js/components/insights/KPI';
+import TimeSeries from 'js/components/insights/charts/library/TimeSeries';
 import HorizontalBarChart from 'js/components/insights/charts/library/HorizontalBarChart';
 
+import { PullRequestMetricsRequest } from 'js/services/api/openapi-client';
+
+import ForSet from 'js/services/api/openapi-client/model/ForSet';
+import MetricID from 'js/services/api/openapi-client/model/MetricID';
+
+import { dateTime, github } from 'js/services/format';
+
+import moment from 'moment';
 import _ from 'lodash';
 
 export default () => [
-    mostActiveDevs
+    mostActiveDevs,
+    prRatioFlow
 ];
+
+const prRatioFlow = {
+    fetcher: async (api, context, data) => {
+        const metricIDs = new MetricID();
+        const forset = ForSet.constructFromObject({
+            repositories: context.repositories,
+            developers: context.contributors
+        });
+
+        const fetchChartsData = async () => {
+            const granularity = 'day';
+            const metrics = [
+                'flow-ratio',
+                'opened',
+                'closed',
+            ].map(metric => metricIDs[metric]);
+
+            const body = new PullRequestMetricsRequest(
+                [forset], metrics,
+                dateTime.ymd(context.interval.from),
+                dateTime.ymd(context.interval.to),
+                granularity, context.account
+            );
+
+            return api.calcMetricsPrLinear(body);
+        };
+
+        const fetchKPIsData = async () => {
+        };
+
+        const chartData = await fetchChartsData();
+        const KPIsData = await fetchKPIsData();
+
+        return Promise.resolve({
+            chartData: chartData,
+            KPIsData: KPIsData
+        });
+    },
+    calculator: (fetched) => ({
+        chartData: _(fetched.chartData.calculated[0].values)
+            .map(v => ({
+                day: v.date,
+                value: (v.values[1] || 1) / (v.values[2] || 1)
+            }))
+            .value(),
+        KPIsData: {
+            avgRatioFlow: {
+                value: _(fetched.chartData.calculated[0].values)
+                    .map(v => (v.values[1] || 1) / (v.values[2] || 1))
+                    .meanBy()
+            },
+            highestDay: _(fetched.chartData.calculated[0].values)
+                .map(v => ({
+                    day: moment(v.date).format('dddd'),
+                    value: (v.values[1] || 1) / (v.values[2] || 1)
+                }))
+                .groupBy('day')
+                .map((series, k) => ({
+                    day: k,
+                    value: _(series)
+                        .map('value')
+                        .mean()
+                }))
+                .maxBy('value')
+        },
+        axisKeys: {
+            x: 'day',
+            y: 'value',
+        },
+        totalPRs: fetched.length
+    }),
+    factory: (computed) => ({
+        meta: {
+            title: 'Pull Request Ratio Flow',
+            description: 'Some description for "Pull Request Ratio Flow"'
+        },
+        content: [
+            {
+                chart: {
+                    component: TimeSeries,
+                    params: {
+                        data: computed.chartData,
+                        extra: {
+                            reference: {
+                                value: 1,
+                                color: '#FFA008'
+                            },
+                            labels: {
+                                y: 'PR Ratio Flow'
+                            },
+                            maxNumberOfTicks: 10,
+                            axisKeys: computed.axisKeys,
+                            color: '#41CED3'
+                        }
+                    }
+                },
+                kpis: [
+                    {
+                        title: {text: 'Average Pull Request', bold: true},
+                        subtitle: {text: 'Ratio Flow'},
+                        component: SimpleKPI,
+                        params: {
+                            value: computed.KPIsData.avgRatioFlow.value.toFixed(2)
+                        }
+                    },
+                    {
+                        title: {text: 'Day with the Highest Pull Request', bold: true},
+                        subtitle: {text: 'Ratio Flow'},
+                        component: SimpleKPI,
+                        params: {
+                            value: `${computed.KPIsData.highestDay.day} (${computed.KPIsData.highestDay.value.toFixed(2)})`
+                        }
+                    },
+                ]
+            }
+        ]
+    })
+};
 
 const mostActiveDevs = {
     fetcher: async (api, context, data) => {
