4d8312a76c89c0a0dc6cfe87b9fa1d688e82d917
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2020-03-31 14:46:28 +0200

    Add definition for chart "Review Activity"
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/js/components/insights/stages/review/reviewActivity.jsx
+++ src/js/components/insights/stages/review/reviewActivity.jsx
@@ -1,5 +1,6 @@
 import { SimpleKPI } from 'js/components/insights/KPI';
 import HorizontalBarChart from 'js/components/insights/charts/library/HorizontalBarChart';
+import BubbleChart from 'js/components/insights/charts/library/BubbleChart';
 
 import { fetchDevsMetrics } from 'js/services/api/index';
 
@@ -8,6 +9,19 @@ import _ from 'lodash';
 const reviewActivity = {
     fetcher: async (api, context, data) => {
         // TODO: call the api to avoid receiving data from outside
+
+        const fetchFirstBox = async () => {
+            const metrics = [
+                'reviews',
+                'prs-created',
+            ];
+
+            return fetchDevsMetrics(
+                api, context.account, context.interval, metrics,
+                { repositories: context.repositories, developers: context.contributors},
+                'developers'
+            );
+        };
 
         const fetchSecondBox = async () => {
             const metrics = [
@@ -23,6 +37,7 @@ const reviewActivity = {
         };
 
         return Promise.resolve({
+            firstBox: await fetchFirstBox(),
             secondBox: await fetchSecondBox(),
             legacyData: data
         });
@@ -54,11 +69,44 @@ const reviewActivity = {
                   reviewsPerc: v.values[0][0] * 100 / totalReviews,
                   prsCommentsPerc: v.values[0][1] * 100 / totalPRsComments
               }))
-              .orderBy(['reviewsPerc'], ['desc'])
+              .orderBy(['prsCommentsPerc'], ['desc'])
               .take(1)
               .nth(0);
 
         return {
+            firstBox: {
+                chartData: _(fetched.firstBox.calculated)
+                    .map(v => ({
+                        developer: _.replace(v.for.developers[0], 'github.com/', ''),
+                        reviews: v.values[0][0],
+                        prs: v.values[0][1],
+                        size: 5
+                    }))
+                    .filter(v => v.reviews > 0 || v.prs > 0)
+                    .orderBy(['reviews'], ['desc'])
+                    .take(10)
+                    .value(),
+                axisKeys: {
+                    x: 'reviews',
+                    y: 'prs',
+                    size: 'size',
+                    label: 'developer'
+                },
+                avatarMapping: _(fetched.legacyData.users)
+                    .reduce((res, v, k) => {
+                        res[_.replace(k, 'github.com/', '')] = v.avatar;
+                        return res;
+                    }),
+                KPIsData: {
+                    sumPrsCreated: _(fetched.firstBox.calculated)
+                        .map(v => v.values[0][1])
+                        .sum(),
+                    sumReviews: _(fetched.firstBox.calculated)
+                        .map(v => v.values[0][0])
+                        .sum(),
+                    avgReviewsPerDev: totalReviews / totalReviewers
+                }
+            },
             secondBox: {
                 chartData: secondBoxChartData,
                 axisKeys: {
@@ -77,63 +125,124 @@ const reviewActivity = {
             }
         };
     },
-    factory: (computed) => ({
-        meta: {
-            title: 'Review Activity',
-            description: 'Some description for "Review Activity"'
-        },
-        content: [
-            {
-                chart: {
-                    component: HorizontalBarChart,
-                    params: {
-                        title: 'Most Active Reviewers',
-                        data: computed.secondBox.chartData,
-                        extra: {
-                            yAxis: {
-                                imageMapping: computed.secondBox.avatarMapping,
-                                imageMask: 'circle'
-                            },
-                            axisKeys: computed.secondBox.axisKeys,
-                            barWidth: 0.6,
-                            series: {
-                                prsCommentsPerc: {
-                                    name: '% Reviews Comments',
-                                    color: '#FC1763',
+    factory: (computed) => {
+        const sumPrsCreated = computed.firstBox.KPIsData.sumPrsCreated;
+        const sumReviews = computed.firstBox.KPIsData.sumReviews;
+        const gcdPrsReviews = gcd(sumPrsCreated, sumReviews);
+
+        return {
+            meta: {
+                title: 'Review Activity',
+                description: 'Some description for "Review Activity"'
+            },
+            content: [
+                {
+                    chart: {
+                        component: BubbleChart,
+                        params: {
+                            title: 'Number of Pull Requests created',
+                            data: computed.firstBox.chartData,
+                            extra: {
+                                useImages: true,
+                                yAxis: {
+                                    imageMapping: computed.secondBox.avatarMapping,
+                                    imageMask: 'circle'
                                 },
-                                reviewsPerc: {
-                                    name: '% Reviews',
-                                    color: '#FFC507',
+                                grouper: computed.firstBox.grouper,
+                                groups: computed.firstBox.groups,
+                                axisKeys: computed.firstBox.axisKeys,
+                                axisLabels: {
+                                    x: 'Number of Reviews',
+                                    y: 'Number of PRs Created'
+                                },
+                                color: '#41CED3',
+                            }
+                        }
+                    },
+                    kpis: [
+                        {
+                            title: {text: 'Average Pull Requests Reviewed', bold: true},
+                            subtitle: {text: 'Per Developer'},
+                            component: SimpleKPI,
+                            params: {
+                                value: computed.firstBox.KPIsData.avgReviewsPerDev
+                            }
+                        },
+                        {
+                            title: {text: 'Ratio of Pull Requests', bold: true},
+                            subtitle: {text: 'Created/Reviewed'},
+                            component: SimpleKPI,
+                            params: {
+                                value: `${sumPrsCreated / gcdPrsReviews}/${sumReviews / gcdPrsReviews}`
+                            }
+                        },
+                    ]
+                },
+                {
+                    chart: {
+                        component: HorizontalBarChart,
+                        params: {
+                            title: 'Most Active Reviewers',
+                            data: computed.secondBox.chartData,
+                            extra: {
+                                yAxis: {
+                                    imageMapping: computed.secondBox.avatarMapping,
+                                    imageMask: 'circle'
+                                },
+                                axisKeys: computed.secondBox.axisKeys,
+                                barWidth: 0.6,
+                                series: {
+                                    prsCommentsPerc: {
+                                        name: '% Reviews Comments',
+                                        color: '#FC1763',
+                                    },
+                                    reviewsPerc: {
+                                        name: '% Reviews',
+                                        color: '#FFC507',
+                                    }
                                 }
                             }
                         }
-                    }
-                },
-                kpis: [
-                    {
-                        title: {text: 'Total Number of Reviewers', bold: true},
-                        component: SimpleKPI,
-                        params: {
-                            value: computed.secondBox.KPIsData.reviewers,
-                        }
                     },
-                    {
-                        title: {text: 'Proportion of Reviews Made By', bold: true},
-                        subtitle: {
-                            text: computed.secondBox.KPIsData.topReviewer ?
-                                computed.secondBox.KPIsData.topReviewer.developer : ''
+                    kpis: [
+                        {
+                            title: {text: 'Total Number of Reviewers', bold: true},
+                            component: SimpleKPI,
+                            params: {
+                                value: computed.secondBox.KPIsData.reviewers,
+                            }
                         },
-                        component: SimpleKPI,
-                        params: {
-                            value: computed.secondBox.KPIsData.topReviewer ?
-                                computed.secondBox.KPIsData.topReviewer.reviewsPerc.toFixed(2) : '',
-                            unit: '%'
-                        }
-                    },
-                ]
-            },
-        ]
-    })
+                        {
+                            title: {text: 'Proportion of Reviews Comments Made By', bold: true},
+                            subtitle: {
+                                text: computed.secondBox.KPIsData.topReviewer ?
+                                    computed.secondBox.KPIsData.topReviewer.developer : ''
+                            },
+                            component: SimpleKPI,
+                            params: {
+                                value: computed.secondBox.KPIsData.topReviewer ?
+                                    computed.secondBox.KPIsData.topReviewer.prsCommentsPerc.toFixed(2) : '',
+                                unit: '%'
+                            }
+                        },
+                    ]
+                },
+            ]
+        };
+    }
+};
+
+const gcd = (x, y) => {
+    if ((typeof x !== 'number') || (typeof y !== 'number'))
+        return false;
+    x = Math.abs(x);
+    y = Math.abs(y);
+    while(y) {
+        var t = y;
+        y = x % y;
+        x = t;
+    }
+    return x;
 };
 
 export default reviewActivity;
