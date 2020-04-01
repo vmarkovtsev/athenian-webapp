5e75de44d52fbb77a744fff56e34f17e60ac9924
David Pordomingo <David.Pordomingo.F@gmail.com>
2020-04-01 12:41:07 +0200

    Fix ENG-376. Ommit variation badge if it's zero
    Signed-off-by: David Pordomingo <David.Pordomingo.F@gmail.com>

--- src/js/components/ui/Badge.jsx
+++ src/js/components/ui/Badge.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import classnames from 'classnames';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretDown, faCaretUp, faEquals } from '@fortawesome/free-solid-svg-icons';
+import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
 
 export const POSITIVE_IS_BETTER = 'positive-variation-is-better'; // default
 export const NEGATIVE_IS_BETTER = 'negative-variation-is-better';
 
-export default ({ value, trend = POSITIVE_IS_BETTER, className }) => {
+export default ({ value, trend = false, className }) => {
   const commonClasses = ['badge', 'font-weight-normal', 'align-middle', 'd-inline-block'];
   let customClasses, icon;
   let suffix = '%';
@@ -20,17 +20,15 @@ export default ({ value, trend = POSITIVE_IS_BETTER, className }) => {
     customClasses = [trend === NEGATIVE_IS_BETTER ? 'badge-danger' : 'badge-success'];
     icon = faCaretUp;
   } else {
-    customClasses = ['badge-secondary'];
-    icon = faEquals;
-    suffix = '';
+    return '';
   }
 
   return (
-    <span className={classnames('badge', className, ...commonClasses, ...customClasses)}>
+    <span className={classnames(className, ...commonClasses, ...customClasses)}>
       {trend ? (
         <>
           <FontAwesomeIcon icon={icon} className="align-bottom" />
-          {value ? <span className="ml-1">{Math.abs(value)}{suffix}</span> : ''}
+          <span className="ml-1">{Math.abs(value)}{suffix}</span>
         </>
       ) : (
           <>{value}</>
