79598e2c89385f72326c91f260a5873bda2ebe8e
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2020-04-05 00:59:28 +0200

    Fix summary chart of Overview section
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/js/pages/pipeline/Overview.jsx
+++ src/js/pages/pipeline/Overview.jsx
@@ -7,8 +7,10 @@ import { usePipelineContext } from 'js/context/Pipeline';
 import PullRequests from 'js/components/pipeline/PullRequests';
 import { SummaryMetrics } from 'js/components/pipeline/StageMetrics';
 import { SmallTitle } from 'js/components/ui/Typography';
+import SummaryChart from 'js/components/pipeline/SummaryChart';
 import Tabs from 'js/components/layout/Tabs';
 
+import { palette } from 'js/res/palette';
 import { dateTime, number } from 'js/services/format';
 
 export default () => {
@@ -24,31 +26,41 @@ export default () => {
         });
     }
 
-    return <>
-        {leadtimeContext.avg ? (
-            <SummaryMetrics conf={leadtimeContext} >
-                {stagesContext.map((stage, i) => stage.overallProportion ? (
-                    <div key={i}>
-                        <div><SmallTitle content={stage.title} /></div>
-                        <span
-                            className={classnames('overall-proportion d-block mb-2', stage.stageName)}
-                            style={{ width: `${100 * stage.overallProportion / slowerStage}%` }}
-                            data-toggle="tooltip"
-                            data-placement="right"
-                            title={`${dateTime.human(stage.avg)} (${number.percentage(stage.overallProportion)})`}
-                        />
-                    </div>
-                ) : null)}
-            </SummaryMetrics>
-        ) : null
-        }
+    const chart = <SummaryChart
+                    name={leadtimeContext.stageName}
+                    metric={leadtimeContext.metric}
+                    config={{
+                        height: 280,
+                        color: palette.stages[leadtimeContext.stageName],
+                        average: leadtimeContext.avg
+                    }}
+                  />;
+    const kpi = stagesContext.map((stage, i) => stage.overallProportion ? (
+        <div key={i}>
+          <div><SmallTitle content={stage.title} /></div>
+          <span
+            className={classnames('overall-proportion d-block mb-2', stage.stageName)}
+            style={{ width: `${100 * stage.overallProportion / slowerStage}%` }}
+            data-toggle="tooltip"
+            data-placement="right"
+            title={`${dateTime.human(stage.avg)} (${number.percentage(stage.overallProportion)})`}
+          />
+        </div>
+    ) : null);
 
-        <Tabs tabs={[
-            {
-                title: 'Pull Requests',
-                badge: prsContext.prs.length,
-                content: <PullRequests data={prsContext} stage="overview" />
-            }
-        ]} />
-    </>
+    return (
+        <>
+          {leadtimeContext.avg ? (
+              <SummaryMetrics conf={leadtimeContext} chart={chart} kpi={kpi} />
+          ) : null}
+
+          <Tabs tabs={[
+              {
+                  title: 'Pull Requests',
+                  badge: prsContext.prs.length,
+                  content: <PullRequests data={prsContext} stage="overview" />
+              }
+          ]} />
+        </>
+    );
 };
