65629dfafb0125b72cd23a01c4aca286ad7c5b0f
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2020-04-21 13:10:43 +0200

    Make DataContext explicitly handle global data
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/js/context/Data.jsx
+++ src/js/context/Data.jsx
@@ -4,27 +4,48 @@ const DataContext = React.createContext({});
 
 export const useDataContext = () => useContext(DataContext);
 
+const globalKeysWhitelist = [
+    'filter.repos',
+    'filter.contribs',
+    'prs'
+];
+
+const globalKeyPrefix = 'global.';
+
 const dataStateReducer = (state, action) => {
     if (action.reset) {
         console.log("Resetting all data");
         return {};
     }
 
-    const { id, data } = action;
-    console.log("Setting data with id: ", id);
-    state[id] = data;
+    const { id, data, global } = action;
+    console.log(`Setting ${global ? 'global ' : ''}data with id: ${id} | ${state[id]}`);
+    if (global) {
+        if (!globalKeysWhitelist.includes(id)) {
+            throw Error(`Trying to use unrecognized global id: ${id}`);
+        } else if (state[id]) {
+            throw Error(`Trying to override global data with id: ${id}`);
+        }
+    } else if (id.startsWith(globalKeyPrefix)) {
+        throw Error(`Non-global data cannot have id with prefix ${globalKeyPrefix}: ${id}`);
+    }
+
+    const prefix = global ? globalKeyPrefix : '';
+    state[`${prefix}${id}`] = data;
     return state;
 };
 
 export default ({ children }) => {
     const [dataState, dispatchDataState] = useReducer(dataStateReducer, {});
 
+    const reset = () => dispatchDataState({reset: true});
     const get = (id) => id ? dataState[id] : null;
     const set = (id, data) => id ? dispatchDataState({id, data}) : null;
-    const reset = () => dispatchDataState({reset: true});
+    const getGlobal = (id) => id ? dataState[`global.${id}`] : null;
+    const setGlobal = (id, data) => id ? dispatchDataState({id, data, global: true}) : null;
 
     return (
-        <DataContext.Provider value={{get, set, reset}}>
+        <DataContext.Provider value={{get, set, getGlobal, setGlobal, reset}}>
             {children}
         </DataContext.Provider >
     );
