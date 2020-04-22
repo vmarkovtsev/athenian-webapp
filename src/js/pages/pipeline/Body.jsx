b85aae1b76e483a4e400c49249be53b91f695e49
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2020-04-22 17:08:36 +0200

    Remove unused contexts usage from Body component
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/js/pages/pipeline/Body.jsx
+++ src/js/pages/pipeline/Body.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { useParams } from "react-router-dom";
 
-import { usePipelineContext } from 'js/context/Pipeline';
 import { useDataContext } from 'js/context/Data';
-import { usePRsContext } from 'js/context/PRs';
 
 import MainMetrics from 'js/components/pipeline/MainMetrics';
 import Thumbnails from 'js/components/pipeline/Thumbnails';
@@ -12,8 +10,6 @@ import { pipelineStagesConf, getStage } from 'js/pages/pipeline/Pipeline';
 
 export default ({ children }) => {
     const { globalDataReady } = useDataContext();
-    const prsContext = usePRsContext();
-    const { stages: stagesContext } = usePipelineContext();
     const { name: stageSlug } = useParams();
     const activeConf = getStage(pipelineStagesConf, stageSlug);
 
@@ -25,7 +21,7 @@ export default ({ children }) => {
     return (
         <>
           <MainMetrics />
-          <Thumbnails prs={prsContext.prs} stages={stagesContext} activeCard={activeConf && activeConf.slug} />
+          <Thumbnails activeCard={activeConf && activeConf.slug} />
           {children}
         </>
     );
