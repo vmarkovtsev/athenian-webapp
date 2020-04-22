d96f107bfc002ec1c3926958f449b04fc2240a06
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2020-04-22 16:25:17 +0200

    Avoid rendering body if the global metrics are not started
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/js/pages/pipeline/Body.jsx
+++ src/js/pages/pipeline/Body.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from "react-router-dom";
 
 import { usePipelineContext } from 'js/context/Pipeline';
+import { useDataContext } from 'js/context/Data';
 import { usePRsContext } from 'js/context/PRs';
 
 import MainMetrics from 'js/components/pipeline/MainMetrics';
@@ -10,10 +11,16 @@ import Thumbnails from 'js/components/pipeline/Thumbnails';
 import { pipelineStagesConf, getStage } from 'js/pages/pipeline/Pipeline';
 
 export default ({ children }) => {
+    const { globalDataReady } = useDataContext();
     const prsContext = usePRsContext();
     const { stages: stagesContext } = usePipelineContext();
     const { name: stageSlug } = useParams();
     const activeConf = getStage(pipelineStagesConf, stageSlug);
+
+    if (!globalDataReady) {
+        console.log("Data not ready yet");
+        return null;
+    }
 
     return (
         <>
