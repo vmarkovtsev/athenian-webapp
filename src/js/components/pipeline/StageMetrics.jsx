12f6734b9e289a0cf676bb8c76d3790d789e21f6
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2020-04-23 11:51:31 +0200

    Make stage metrics and overview metrics componet use global data
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/js/components/pipeline/StageMetrics.jsx
+++ src/js/components/pipeline/StageMetrics.jsx
@@ -1,33 +1,131 @@
 import React from 'react';
 import classnames from 'classnames';
-
+import FilledAreaChart from 'js/components/charts/FilledAreaChart';
 import Badge from 'js/components/ui/Badge';
+import { useApi } from 'js/hooks';
 import { BigNumber } from 'js/components/ui/Typography';
+import { pipelineStagesConf } from 'js/pages/pipeline/Pipeline';
 import { SmallTitle } from 'js/components/ui/Typography';
-
+import DataWidget from 'js/components/DataWidget';
 import { dateTime, number } from 'js/services/format';
-
+import { palette } from 'js/res/palette';
 import { NEGATIVE_IS_BETTER } from 'js/components/ui/Badge';
+import _ from "lodash";
 
-export const SummaryMetrics = ({ conf, chart, kpi }) => {
+export const StageSummaryMetrics = ({name, stage}) => {
+    const metric = stage.metric;
+    const { ready: apiReady, context: apiContext } = useApi();
+
+    if (!apiReady) {
+        return null;
+    }
+
+    const plumber = (data) => {
+        const proportion = (
+            data.global['prs-metrics.values'].all[metric] /
+                data.global['prs-metrics.values'].all['cycle-time'] * 100
+        );
+
+        return (
+            {
+                kpisData: stage.summary(proportion, data.global.prs.prs, apiContext.interval),
+                average: data.global['prs-metrics.values'].all[metric],
+                variation: data.global['prs-metrics.variations'][metric],
+                timeseries: _(data.global['prs-metrics.values'].custom[metric])
+                    .map(d => ({x: d.date, y: d.value}))
+                    .value()
+            }
+        );
+    };
+
+    const chartConfig = {
+        height: 280,
+        color: palette.stages[name],
+    };
+
+    const config = {
+        chartConfig,
+        KPIComponent: StageSummaryKPI,
+        stage
+    };
+
+    return (
+        <DataWidget
+          id={`summary-chart-${name}`}
+          component={SummaryMetrics} plumber={plumber}
+          globalDataIDs={['prs', 'prs-metrics.values', 'prs-metrics.variations']}
+          config={config}
+        />
+    );
+};
+
+export const OverviewSummaryMetrics = ({name, metric}) => {
+
+    const plumber = (data) => {
+        const cycleTime = data.global['prs-metrics.values'].all['cycle-time'];
+        const proportions = _(data.global['prs-metrics.values'].all)
+              .pick(['wip-time', 'review-time', 'merging-time', 'release-time'])
+              .mapValues(v => v * 100 / cycleTime)
+              .value();
+        const fastest = _(proportions).values().max();
+        const normalizedProportions = _(proportions)
+              .mapValues(v => v / fastest * 100)
+              .value();
+
+        return (
+            {
+                kpisData: {
+                    normalizedProportions
+                },
+                average: data.global['prs-metrics.values'].all[metric],
+                variation: data.global['prs-metrics.variations'][metric],
+                timeseries: _(data.global['prs-metrics.values'].custom[metric])
+                    .map(d => ({x: d.date, y: d.value}))
+                    .value()
+            }
+        );
+    };
+
+    const chartConfig = {
+        height: 280,
+        color: palette.stages[name],
+    };
+
+    const config = {
+        chartConfig,
+        KPIComponent: OverviewStageSummaryKPI,
+        stage: pipelineStagesConf[0]
+    };
+
+    return (
+        <DataWidget
+          id={`summary-chart-${name}`}
+          component={SummaryMetrics} plumber={plumber}
+          globalDataIDs={['prs-metrics.values', 'prs-metrics.variations']}
+          config={config}
+        />
+    );
+};
+
+const SummaryMetrics = ({ data, stage, KPIComponent, chartConfig }) => {
   return (
-    <div className={classnames('summary-metric card mb-4 px-2', conf.stageName)}>
+    <div className={classnames('summary-metric card mb-4 px-2', stage.stageName)}>
       <div className="card-body">
         <div className="row">
           <div className="col-4">
-            <header className="font-weight-bold text-lg mt-2">{conf.summaryMetricTitle || conf.title}</header>
+            <header className="font-weight-bold text-lg mt-2">{stage.summaryMetricTitle || stage.title}</header>
             <div className="pl-2">
               <div className="font-weight-bold mt-4 mb-3 pb-2 border-bottom">
-                <BigNumber content={dateTime.human(conf.avg)} isXL />
-                <Badge value={number.round(conf.variation)} className="ml-2" trend={NEGATIVE_IS_BETTER} />
+                <BigNumber content={dateTime.human(data.average * 1000)} isXL />
+                <Badge value={number.round(data.variation)} className="ml-2" trend={NEGATIVE_IS_BETTER} />
               </div>
               <div>
-                {kpi}
+                <KPIComponent data={data.kpisData} />
               </div>
             </div>
           </div>
           <div className="col-8 align-self-center">
-            {chart}
+            <FilledAreaChart data={{timeseries: data.timeseries, average: data.average}} {...chartConfig}/>
           </div>
         </div>
       </div>
@@ -35,7 +133,7 @@ export const SummaryMetrics = ({ conf, chart, kpi }) => {
   );
 };
 
-export const StageSummaryKPI = ({ data }) => {
+const StageSummaryKPI = ({ data }) => {
   return (
     <div className="row">
       {data.map((kpi, key) =>
@@ -46,4 +144,24 @@ export const StageSummaryKPI = ({ data }) => {
       )}
     </div>
   );
+};
+
+const OverviewStageSummaryKPI = ({data}) => {
+    const stages = pipelineStagesConf.slice(2, pipelineStagesConf.length);
+    return (
+        <>
+          {stages.map((stage, i) => (
+              <div key={i}>
+                <div><SmallTitle content={stage.title} /></div>
+                <span
+                  className={classnames('overall-proportion d-block mb-2', stage.stageName)}
+                  style={{ width: `${data.normalizedProportions[stage.metric]}%` }}
+                  data-toggle="tooltip"
+                  data-placement="right"
+                  title={`${dateTime.human(stage.avg)} (${number.percentage(stage.overallProportion)})`}
+                />
+              </div>
+          ))}
+        </>
+    );
 };
