a867be38cf3d00f008c900a8e0633478246d5b6a
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2020-04-29 14:16:38 +0200

    Fix test
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/js/Routes.test.js
+++ src/js/Routes.test.js
@@ -3,19 +3,29 @@ import { act } from 'react-dom/test-utils';
 import ReactDOM from 'react-dom';
 import Routes from 'js/Routes';
 
+import {
+    Router,
+} from 'react-router-dom';
+
+import history from 'js/services/history';
 import * as Auth0 from 'js/context/Auth0';
 
 it('renders without crashing', () => {
 
-  jest
-    .spyOn(Auth0, 'useAuth0')
-    .mockImplementation(() => ({}))
+    jest
+        .spyOn(Auth0, 'useAuth0')
+        .mockImplementation(() => ({}));
 
-  window.ENV = { api: {} };
+    window.ENV = { api: {}, environment: 'development'};
 
-  const div = document.createElement('div');
-  act(() => {
-    ReactDOM.render(<Routes />, div);
-  });
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    act(() => {
+        ReactDOM.render(<>
+        <Router history={history}>
+          <Routes />
+        </Router>
+      </>, div);
+    });
+
+    ReactDOM.unmountComponentAtNode(div);
 });
