54758f2de6c54803442a6d8d53f149a46019c0d5
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2020-04-30 18:37:20 +0200

    Indent Callback.jsx
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/js/pages/auth/Callback.jsx
+++ src/js/pages/auth/Callback.jsx
@@ -9,89 +9,89 @@ import { buildApi } from 'js/services/api';
 import InvitationLink from 'js/services/api/openapi-client/model/InvitationLink';
 
 export default () => {
-  const invitationAcceptRequest = useRef(false);
-  const { loading, isAuthenticated, getTokenSilently, logout } = useAuth0();
-  const [redirectTo, setRedirectTo] = useState(null);
-  const [errorMessage, setErrorMessage] = useState('');
-  const query = new URLSearchParams(useLocation().search);
+    const invitationAcceptRequest = useRef(false);
+    const { loading, isAuthenticated, getTokenSilently, logout } = useAuth0();
+    const [redirectTo, setRedirectTo] = useState(null);
+    const [errorMessage, setErrorMessage] = useState('');
+    const query = new URLSearchParams(useLocation().search);
 
-  useEffect(() => {
-    if (loading || !isAuthenticated || invitationAcceptRequest.current) {
-      return;
-    };
+    useEffect(() => {
+        if (loading || !isAuthenticated || invitationAcceptRequest.current) {
+            return;
+        };
 
-    getTokenSilently()
-      .then(async token => {
-        const inviteLink = query.get('inviteLink');
+        getTokenSilently()
+            .then(async token => {
+                const inviteLink = query.get('inviteLink');
 
-        if (!inviteLink) {
-          setRedirectTo({
-            pathname: query.get('targetUrl') || '/',
-          });
-          return;
-        }
+                if (!inviteLink) {
+                    setRedirectTo({
+                        pathname: query.get('targetUrl') || '/',
+                    });
+                    return;
+                }
 
-        try {
-          invitationAcceptRequest.current = true;
-          const check = await acceptInvite(token, inviteLink);
-          setRedirectTo({
-            pathname: check.type === 'admin' ? '/waiting' : '/stage/overview',
-            state: { origin: FROM_REGISTRATION },
-          });
-        } catch (err) {
-          setErrorMessage(err.message);
+                try {
+                    invitationAcceptRequest.current = true;
+                    const check = await acceptInvite(token, inviteLink);
+                    setRedirectTo({
+                        pathname: check.type === 'admin' ? '/waiting' : '/stage/overview',
+                        state: { origin: FROM_REGISTRATION },
+                    });
+                } catch (err) {
+                    setErrorMessage(err.message);
 
-          // TODO (dpordomingo):
-          // This is needed because if the invitation fails, the user should not be logged in.
-          // Also, the logout is delayed because Auth0 redirects to the 'logoutRedirectUri'
-          // inmediately, and then the user would not be able to see the error message.
-          // Workaround: store the error, let Auth0 to redirect, and then show the stored message.
-          window.setTimeout(() => logout({ returnTo: window.ENV.auth.logoutRedirectUri }), 3000);
-          return;
-        }
-      });
-  }, [loading, isAuthenticated, getTokenSilently, logout, query]);
+                    // TODO (dpordomingo):
+                    // This is needed because if the invitation fails, the user should not be logged in.
+                    // Also, the logout is delayed because Auth0 redirects to the 'logoutRedirectUri'
+                    // inmediately, and then the user would not be able to see the error message.
+                    // Workaround: store the error, let Auth0 to redirect, and then show the stored message.
+                    window.setTimeout(() => logout({ returnTo: window.ENV.auth.logoutRedirectUri }), 3000);
+                    return;
+                }
+            });
+    }, [loading, isAuthenticated, getTokenSilently, logout, query]);
 
-  if (errorMessage) {
-    return <Simple>{errorMessage}</Simple>;
-  }
+    if (errorMessage) {
+        return <Simple>{errorMessage}</Simple>;
+    }
 
-  if (loading) {
-    return <Simple>Loading...</Simple>;
-  }
+    if (loading) {
+        return <Simple>Loading...</Simple>;
+    }
 
-  if (redirectTo?.pathname) {
-    return <Redirect to={redirectTo} />;
-  }
+    if (redirectTo?.pathname) {
+        return <Redirect to={redirectTo} />;
+    }
 
-  return (
-    <Simple>
-      {isAuthenticated ? 'Authenticated' : 'Not authenticated'}
-    </Simple>
-  );
+    return (
+        <Simple>
+          {isAuthenticated ? 'Authenticated' : 'Not authenticated'}
+        </Simple>
+    );
 };
 
 const acceptInvite = async (token, inviteLink) => {
-  const api = buildApi(token);
-  const body = new InvitationLink(inviteLink);
+    const api = buildApi(token);
+    const body = new InvitationLink(inviteLink);
 
-  let check;
-  try {
-    check = await api.checkInvitation(body);
-  } catch (err) {
-    throw new Error(`Error reading the invitation ${err.error.message}`);
-  }
+    let check;
+    try {
+        check = await api.checkInvitation(body);
+    } catch (err) {
+        throw new Error(`Error reading the invitation ${err.error.message}`);
+    }
 
-  if (!check.valid || !check.active) {
-    const cause = !check.valid ? 'valid' : 'active';
-    throw new Error(`Invitation is not ${cause}`);
-  }
+    if (!check.valid || !check.active) {
+        const cause = !check.valid ? 'valid' : 'active';
+        throw new Error(`Invitation is not ${cause}`);
+    }
 
-  try {
-    await api.acceptInvitation(body);
-  } catch (err) {
-    throw new Error(`Could not accept the invitation. Err#${err.body.status} ${err.body.type}. ${err.body.detail}`);
-  }
+    try {
+        await api.acceptInvitation(body);
+    } catch (err) {
+        throw new Error(`Could not accept the invitation. Err#${err.body.status} ${err.body.type}. ${err.body.detail}`);
+    }
 
-  return check;
+    return check;
 };
