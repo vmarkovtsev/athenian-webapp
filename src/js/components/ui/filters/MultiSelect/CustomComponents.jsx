1835c745da67f6512fa61626507914de5f4df85f
Ricardo <navas.souza@gmail.com>
2020-05-14 23:20:34 -0300

    fix(ENG-852): Remove the shadow effect of the check-boxes that breaks vertical alignment

--- src/js/components/ui/filters/MultiSelect/CustomComponents.jsx
+++ src/js/components/ui/filters/MultiSelect/CustomComponents.jsx
@@ -71,7 +71,7 @@ export const Dropdown = ({
  */
 const Checkbox = ({ isChecked }) => {
   return (
-    <svg width="20px" height="20px" viewBox="0 0 20 20" className="mr-2">
+    <svg width="18px" height="18px" viewBox="0 0 18 18" className="mr-2">
       <rect stroke="#D6DBE4" strokeWidth="1" x="0" y="0" width="18" height="18" fill="#fff"></rect>
       { isChecked &&
         <polygon fill="#24C7CC" points="4.66692304 8.35872968 7.13673213 10.8903181 14.4025708 4 16 5.49137391 7.13673213 14 3 9.96982087"></polygon>
