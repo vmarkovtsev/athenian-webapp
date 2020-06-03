cc73fe52976d5bd3b9fc8b6d68d292a881336de5
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2020-06-03 19:46:10 +0200

    Rename function callback onAccept to onApply in FilterFooter
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/js/components/ui/filters/FilterFooter.jsx
+++ src/js/components/ui/filters/FilterFooter.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import classnames from 'classnames'
 
-export const FilterFooter = ({ onCancel, onAccept, isAcceptable, extra }) => (
+export const FilterFooter = ({ onCancel, onApply, isAcceptable, extra }) => (
   <div className="bg-white filter-buttons border-top px-3 py-2 d-flex align-items-center">
     <div>
     {extra}
@@ -10,7 +10,7 @@ export const FilterFooter = ({ onCancel, onAccept, isAcceptable, extra }) => (
       <button onClick={onCancel} className="btn btn-link text-secondary px-3">Cancel</button>
       <button
         disabled={!isAcceptable}
-        onClick={onAccept}
+        onClick={onApply}
         className={classnames('btn btn-orange px-3', !isAcceptable && 'btn-disabled')}
       >
         Apply
